68d3ff1128146bbbe0f1d232acb6c80ac6762926
Sejal0410 <sejalpawar0410@gmail.com>
2025-03-15 22:45:21 +0000

    a

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -29,7 +29,7 @@ vector<int> dijkstra_shortest_path(const Graph &G, int source, vector<int> &prev
     return d;
 }
 
-vector<int> extract_shortest_path(const vector<int> &previous, int destination){
+vector<int> extract_shortest_path(const vector<int> &distances, const vector<int> &previous, int destination){
     vector<int> path;
     if(destination == -1){
         return path;
